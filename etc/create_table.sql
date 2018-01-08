3ac1cd38209e0df2b5ab74d682e2b3203cb27147
DingJunyao <billgatessnagit@foxmail.com>
2018-01-08 20:15:18 +0800

    rewrite sql & update md

--- etc/create_table.sql
+++ etc/create_table.sql
@@ -1,67 +1,83 @@
 CREATE TABLE `goods`(
-	`goodsid` VARCHAR(4) NOT NULL,
+	`gid` VARCHAR(13) NOT NULL,
 	`gname` VARCHAR(30) NOT NULL,
-	`class` VARCHAR(20) NOT NULL,
-	`brand` VARCHAR(20) NOT NULL,
-	`price` DECIMAL(5,2) NOT NULL,
-	`place` VARCHAR(30) NOT NULL,
-	`note` VARCHAR(40),
-	PRIMARY KEY (`goodsid`)
+	`gclass` VARCHAR(20) NOT NULL,
+	`gbrand` VARCHAR(20),
+	`gprice` DECIMAL(6,2) NOT NULL,
+	`gunit` VARCHAR(8) NOT NULL,
+	`gshelf` INT(4),
+	`gplace` VARCHAR(30),
+	`gnote` VARCHAR(40),
+	PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `supplier`(
-	`supplierid` VARCHAR(8) NOT NULL,
-	`sname` VARCHAR(36) NOT NULL,
-	`phone` VARCHAR(15) NOT NULL,
+	`sid` VARCHAR(5) NOT NULL,
+	`sname` VARCHAR(20) NOT NULL,
+	`sphone` VARCHAR(15) NOT NULL,
 	`saddress` VARCHAR(40) NOT NULL,
-	`note` VARCHAR(40),
-	PRIMARY KEY (`supplierid`)
+	`snote` VARCHAR(40),
+	PRIMARY KEY (`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `warehouse`(
-	`warehouseid` INT(2) NOT NULL,
-	`wname` VARCHAR(20) NOT NULL,
-	`waddress` VARCHAR(40) NOT NULL,
-	`note` VARCHAR(40),
-	PRIMARY KEY (`warehouseid`)
+	`hid` VARCHAR(2) NOT NULL,
+	`hname` VARCHAR(20) NOT NULL,
+	`hnote` VARCHAR(40),
+	PRIMARY KEY (`hid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE order(
-	`orderid` VARCHAR(11) NOT NULL,
-	`way` VARCHAR(20) NOT NULL,
-	`quantity` INT(4) NOT NULL,
-	`date` date NOT NULL,
-	`supplierid` INT(5) NOT NULL,
-	`goodid` INT(4) NOT NULL,
-	`note` VARCHAR(),
-	PRIMARY KEY (`orderid`),
-	constraint order_goodsid_fk foreign key (goodsid) references goods(goodsid),
-	constraint order_supplierid_fk foreign key (supplierid) references supplier(supplierid)
+CREATE TABLE `order`(
+	`oid` VARCHAR(12) NOT NULL,
+	`ogid` VARCHAR(9) NOT NULL,
+	`otime` DATETIME NOT NULL,
+	`sid` VARCHAR(5) NOT NULL,
+	`gid` VARCHAR(13) NOT NULL,
+	`onumber` INT(4) NOT NULL,
+	`onote` VARCHAR(40),
+	PRIMARY KEY (`oid`),
+	CONSTRAINT o_gid_fk FOREIGN KEY (`gid`) REFERENCES goods(`gid`),
+	CONSTRAINT o_sid_fk FOREIGN KEY (`sid`) REFERENCES supplier(`sid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE warehousewarrant(
-	`warehousewarrant` INT(8) NOT NULL,
-	`warehouseid` INT(2) NOT NULL,
-	`goodsid` INT(4) NOT NULL,
-	`date` DATE NOT NULL,
-	`quantity` INT(4) NOT NULL,
-	`note` VARCHAR(40),
-	PRIMARY KEY (`warehousewarrant`),
-	constraint warehousewarrant_goodsid_fk foreign key (goodsid) references goods(goodsid),
-	constraint warehousewarrant_warehouseid_fk foreign key (warehouseid) references warehouse(warehouseid)
+CREATE TABLE `wrecord`(
+	`rid` VARCHAR(12) NOT NULL,
+	`rgid` VARCHAR(9) NOT NULL,
+	`rtype` INT(1) NOT NULL,
+	`hid` VARCHAR(2) NOT NULL,
+	`gid` VARCHAR(13) NOT NULL,
+	`rtime` DATETIME NOT NULL,
+	`rnumber` INT(4) NOT NULL,
+	`rnote` VARCHAR(40),
+	PRIMARY KEY (`rid`),
+	constraint r_gid_fk foreign key (`gid`) references goods(`gid`),
+	constraint r_hid_fk foreign key (`hid`) references warehouse(`hid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE repertory(
-	`goodsid` INT(4) NOT NULL,
-	`warehouseid` INT(2) NOT NULL,
-	`quantity` INT(4) NOT NULL,
-	`note` VARCHAR(40),
-	PRIMARY KEY (`goodsid`),
-	constraint repertory_warehouseid_fk foreign key (warehouseid) references warehouse(warehouseid)
+CREATE TABLE `wwarrant`(
+	`gid` VARCHAR(13) NOT NULL,
+	`hid` VARCHAR(2) NOT NULL,
+	`wnumber` DECIMAL(7,3) NOT NULL,
+	`wnote`	VARCHAR(40),
+	PRIMARY KEY (`gid`,`hid`),
+	constraint w_gid_fk foreign key (`gid`) references goods(`gid`),
+	constraint w_hid_fk foreign key (`hid`) references warehouse(`hid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE administrator(
-	`administrator` VARCHAR(15) NOT NULL,
-	`key` VARCHAR(64) NOT NULL,
-	PRIMARY KEY (`administrator`),
+CREATE TABLE `import`(
+	`gid` VARCHAR(13) NOT NULL,
+	`sid` VARCHAR(5) NOT NULL,
+	`iprice` DECIMAL(6,2) NOT NULL,
+	`inote`	VARCHAR(40),
+	PRIMARY KEY (`gid`,`sid`),
+	constraint i_gid_fk foreign key (`gid`) references goods(`gid`),
+	constraint i_sid_fk foreign key (`sid`) references supplier(`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user`(
+	`uid` VARCHAR(15) NOT NULL,
+	`uhash` VARCHAR(64) NOT NULL,
+	`uname` VARCHAR(20) NOT NULL,
+	`utype` INT(2) NOT NULL,
+	PRIMARY KEY (`uid`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
